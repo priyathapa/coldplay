a140f56ae0957f3761705aa413c0af48d9817188
priyathapa <47230449+priyathapa@users.noreply.github.com>
2025-01-30 13:34:47 -0600

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,4 +1,3 @@
-
 var dateToday = new Date();
 var year = dateToday.getFullYear();
 var month = dateToday.getMonth();
@@ -8,6 +7,15 @@ var date = dateToday.getDate();
  var remainingDays = totalDaysInCurrentMonth - date + 9;
  var remainingMonth = 4 - month;
 
- document.getElementById("months").textContent = remainingMonth; // Update months
+ if (remainingMonth <= 0) {
+    remainingMonth = 0; // Set months left to 0
+    document.getElementById("months").textContent = "0"; // Display 0 months left in the HTML
+} else {
+    document.getElementById("months").textContent = remainingMonth; // Update months if there are months left
+}
+
  document.getElementById("days").textContent = remainingDays; // Update days
  console.log(remainingDays, "Days", remainingMonth, "Months left!!");
+
+
+ 
