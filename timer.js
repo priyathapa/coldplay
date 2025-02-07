e7ad04e6b8086765c8c51f81da2dea8724c6ebbb
priyathapa <47230449+priyathapa@users.noreply.github.com>
2025-02-07 14:08:31 -0600

    Update timer.js

--- timer.js
+++ timer.js
@@ -3,7 +3,7 @@ var year = dateToday.getFullYear();
 var month = dateToday.getMonth();
 var date = dateToday.getDate();
 
- var totalDaysInCurrentMonth = new Date(year, month, 0).getDate();
+ var totalDaysInCurrentMonth = new Date(year, month + 1, 0).getDate();
  var remainingDays = totalDaysInCurrentMonth - date + 9;
  var remainingMonth = 4 - month;
 
